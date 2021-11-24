c7e1d13c709151b77bfcb01b404eb7d8aa67b20e
FlyingDancing-Shark <58820486+FlyingDancing-Shark@users.noreply.github.com>
2021-11-24 11:45:44 +0800

    Update DeliveryNotificationManager.cpp

--- DeliveryNotificationManager.cpp
+++ DeliveryNotificationManager.cpp
@@ -8,3 +8,34 @@ InFlightPacket* DeliveryNotificationManager::WriteSequenceNumber(
     return &mInFlightPackets.back();
 } 
 
+
+bool DeliveryNotificationManager::ProcessSequenceNumber(
+                                      InputMemoryBitStream& inPacket)
+{
+     PacketSequenceNumber sequenceNumber;
+     inPacket.Read(sequenceNumber);
+     if(sequenceNumber == mNextExpectedSequenceNumber)
+     {
+//is this expected? add ack to the pending list and process packet
+         mNextExpectedSequenceNumber = sequenceNumber + 1;
+         AddPendingAck(sequenceNumber); 
+         return true;
+     }
+ //is sequence number < current expected? Then silently drop old packet.
+     else if(sequenceNumber < mNextExpectedSequenceNumber)
+     {
+        return false;
+     }
+ //otherwise, we missed some packets
+     else if(sequenceNumber > mNextExpectedSequenceNumber)
+    {
+      //consider all skipped packets as dropped, so
+      //our next expected packet comes after this one ...
+         mNextExpectedSequenceNumber = sequenceNumber + 1;
+ //add an ack for the packet and process it
+//when the sender detects break it acks, it can resend
+         AddPendingAck(sequenceNumber);
+         return true;
+    }
+} 
+
