06787c7efd8ff604c74033c84fe7a89cb24b98ce
FlyingDancing-Shark <58820486+FlyingDancing-Shark@users.noreply.github.com>
2021-11-24 09:38:51 +0800

    Update DeliveryNotificationManager.cpp

--- DeliveryNotificationManager.cpp
+++ DeliveryNotificationManager.cpp
@@ -1,9 +1,10 @@
 InFlightPacket* DeliveryNotificationManager::WriteSequenceNumber(
  OutputMemoryBitStream& inPacket)
 {
- PacketSequenceNumber sequenceNumber = mNextOutgoingSequenceNumber++;
- inPacket.Write(sequenceNumber);
- ++mDispatchedPacketCount;
- mInFlightPackets.emplace_back(sequenceNumber);
- return &mInFlightPackets.back();
- } 
+    PacketSequenceNumber sequenceNumber = mNextOutgoingSequenceNumber++;
+    inPacket.Write(sequenceNumber);
+    ++mDispatchedPacketCount;
+    mInFlightPackets.emplace_back(sequenceNumber);
+    return &mInFlightPackets.back();
+} 
+
